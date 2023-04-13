e70364897117227d05d3118465541e3d7619f567
Julien1703 <julien.offray@gmail.com>
2023-04-13 17:34:41 +0200

    homework

---
+++ symbol.js
@@ -0,0 +1,16 @@
+let x = 250;
+
+function setup() {
+  createCanvas(1000, 1000);
+  }
+
+function draw() {
+  background(200);
+  circle(x+170,150, 15);
+  rect(x, 150, 200, 120, 15);
+  rect(x,190,200,10);
+  circle(x+150,200, 60);
+  circle(x+150,200, 30);
+  rect(x+15,165,30,10,15);
+  rect(x+15,137,45,13);
+}
